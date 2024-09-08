e8039288d3dea030d5658d866d45ac380bb8c396
Mr'Atsadawut <ng.atsadawut@gmail.com>
2024-09-08 16:04:25 +0700

    feat; add http status for make sense

--- src/main/java/com/tjtech/authen/controller/UserController.java
+++ src/main/java/com/tjtech/authen/controller/UserController.java
@@ -3,7 +3,9 @@ package com.tjtech.authen.controller;
 import com.tjtech.authen.entity.User;
 import com.tjtech.authen.security.JwtTokenProvider;
 import com.tjtech.authen.service.UserService;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -28,13 +30,15 @@ public class UserController {
     private JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user) {
+    public ResponseEntity<?> register(@RequestBody User user) {
         User newUser = userService.register(user);
-        return ResponseEntity.ok(newUser);
+//        return ResponseEntity.ok(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> login(@RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<?> login(@RequestParam @NotEmpty(message = "Username is required") String username,
+                                                     @RequestParam @NotEmpty(message = "Password is required") String password) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(username, password));
 
@@ -49,35 +53,42 @@ public class UserController {
         response.put("token", jwt);
         response.put("user", user);
 
-        return ResponseEntity.ok(response);
+//        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<User> updateData(@PathVariable Long id, @RequestBody User updatedUser) {
+    public ResponseEntity<?> updateData(@PathVariable Long id, @RequestBody User updatedUser) {
         User user = userService.updateData(id, updatedUser);
         if (user != null) {
-            return ResponseEntity.ok(user);
+//            return ResponseEntity.ok(user);
+            return ResponseEntity.status(HttpStatus.OK).body(user);
         } else {
-            return ResponseEntity.notFound().build();
+//            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
     }
 
     @PutMapping("/change-password/{id}")
-    public ResponseEntity<Void> changePassword(@PathVariable Long id, @RequestParam String oldPassword, @RequestParam String newPassword) {
+    public ResponseEntity<?> changePassword(@PathVariable Long id, @RequestParam String oldPassword, @RequestParam String newPassword) {
         if (userService.changePassword(id, oldPassword, newPassword)) {
-            return ResponseEntity.ok().build();
+//            return ResponseEntity.ok().build();
+            return ResponseEntity.status(HttpStatus.OK).body("Password has been changed success!");
         } else {
-            return ResponseEntity.status(400).build();
+//            return ResponseEntity.status(400).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         if (userService.deleteUser(id)) {
-            return ResponseEntity.ok().build();
+//            return ResponseEntity.ok().build();
+            return ResponseEntity.status(HttpStatus.OK).body("User has been successfully deleted.");
         } else {
-            return ResponseEntity.notFound().build();
+//            return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
     }
 }
